bbe01ecc7374b7f1b7ef9f543c5fa379f89016c6
mandajm <etudiant42@sayna.io>
2019-03-11 09:08:16 -0400

    ajout des validations pour la table item

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,6 @@
 class Item < ApplicationRecord
+	validates :title, presence: true, length: { minimum: 5},
+	validates :price, presence: true, numericality: true
+	validates :description, presence: true, length: {minimum: 5, maximum: 500}
+	validates :image_url, uniqueness: true, presence: true
 end
