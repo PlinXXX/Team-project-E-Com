1a6190e73c57479e22297f85e148a49bfa2fbc0e
santatramaximin <santatramaximin@gmail.com>
2019-03-11 16:25:59 +0100

    Header need some upgrades

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
+  
+  get '/presentation/team'
 	root 'home#index'
-	resources :home
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
