9ca82ce0f430f8207d32d6e666ca77208f8e8cfd
santatramaximin <santatramaximin@gmail.com>
2019-03-12 13:04:13 +0100

    Pour vous

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,15 +2,5 @@ class HomeController < ApplicationController
   def index
   	@items = Item.all
   end
-
-  def show
-    @id = params[:id]
-    @item = Item.find(@id)
-    puts "*"*100
-    puts @item.title
-
-    puts "*"*100
-  end
-
 end
 
--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,8 @@
 class ItemsController < ApplicationController
-
 	before_action :authenticate_user!, expect: [:index, :show]
+
+	def show
+    @id = params[:id]
+    @item = Item.find(@id)
+  end
 end
